f12f93ef0a4b78342ad94fa5b53896713edca6d8
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-03-25 15:06:31 +0800

    添加导航定位

--- main.js
+++ main.js
@@ -19,3 +19,21 @@ for (let i = 0; i < liTags.length; i++) {
         leaveEvent.currentTarget.classList.remove('active')
     }
 }
+
+// 锚点定位
+let aTags = document.querySelectorAll('.topNavBar nav > ul > li > a')
+for (let i = 0; i < aTags.length; i++) {
+    aTags[i].onclick = function (clickEvent) {
+        clickEvent.preventDefault()     // 禁用默认锚点跳转，导航栏会遮挡
+        let a = clickEvent.currentTarget
+        console.log(a);
+        
+
+        // a.href是浏览器的API，是带HTTP协议的；
+        // a.getAttribute('href')是用户写入的，只有锚点部分
+        let href = a.getAttribute('href')
+        let element = document.querySelector(href)
+        let top = element.offsetTop
+        window.scrollTo(0, top - 100)
+    }
+}
