7da950321fccd12bb7309bc126660aa0725493a5
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-06-07 00:16:40 +0800

    封装sticky-topbar.js

--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -1,7 +1,25 @@
 !function () {
     var view = document.querySelector('#topNavBar')
-    window.addEventListener('scroll', () => {
-        // 添加屏幕滚动的导航栏动画
-        window.scrollY ? view.classList.add('sticky') : view.classList.remove('sticky')
-    })
+
+    var controller = {
+        view: null,
+        init: function (view) {
+            this.view = view
+            this.bindEvents()
+        },
+        bindEvents: function () {
+            window.addEventListener('scroll', () => {
+                // 添加屏幕滚动的导航栏动画
+                window.scrollY ? this.active() : this.deactive()
+            })
+        },
+        active: function () {
+            view.classList.add('sticky')
+        },
+        deactive: function () {
+            view.classList.remove('sticky')
+        },
+    }
+
+    controller.init(view)
 }.call()
