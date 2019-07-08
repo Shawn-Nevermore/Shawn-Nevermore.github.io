2fd713717cc293ddd6da42d0bcef0f45fc0a60d2
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-07-08 09:51:09 +0800

    更新留言板，添加npx parcel包

--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -9,6 +9,7 @@
         },
         bindEvents: function () {
             window.addEventListener('scroll', () => {
+                
                 // 添加屏幕滚动的导航栏动画
                 window.scrollY ? this.active() : this.deactive()
             })
