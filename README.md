085f90baa5c568a13407bbfaa0ab1e59723dc826
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-08-09 19:16:27 +0800

    批量更新代码格式

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 > This is a link to my personal website: uiao.info# Shawn-Nevermore.github.io
 
-##  个人博客系统
+## 个人博客系统
+
 ### （1）欢迎页动画
+
 ### （2）导航栏
+
 ### （3）名片（含展示外链、简历下载、个人简介）
+
 ### （4）技能
+
 ### （5）作品集
 
 待开发：
+
 1.  博客页
 2.  背景音乐页
 3.  下拉日历
@@ -15,9 +21,10 @@
 5.  作品集跳转
 
 uiao.info 下的其他项目一览：
--   阴阳八卦：http://uiao.info/YinYang
--   我的书签：https://uiao.info/myBookmarks
--   手机画板：https://uiao.info/canvas-demo
--   轮播（回家的诱惑）：https://uiao.info/swiper-demo
--   无缝轮播：https://uiao.info/swiper-demo/my-infinity-carousel
--   苹果风格轮播：https://uiao.info/swiper-demo/apple-like-gallery
+
+- 阴阳八卦：http://uiao.info/YinYang
+- 我的书签：https://uiao.info/myBookmarks
+- 手机画板：https://uiao.info/canvas-demo
+- 轮播（回家的诱惑）：https://uiao.info/swiper-demo
+- 无缝轮播：https://uiao.info/swiper-demo/my-infinity-carousel
+- 苹果风格轮播：https://uiao.info/swiper-demo/apple-like-gallery
--- myBookmarks/main.js
+++ myBookmarks/main.js
@@ -1,7 +1,7 @@
 // 1.初始化数据
 var key2site = init()
 var keys = key2site['keys'],
-    hash = key2site['hash']
+  hash = key2site['hash']
 
 // 2.生成键盘
 generateKeyboard(keys, hash)
@@ -9,153 +9,146 @@ generateKeyboard(keys, hash)
 // 3.监听键盘
 listenToUser(hash)
 
-
 // 工具函数
 function getFromLocalStorage(name) {
-    return JSON.parse(localStorage.getItem(name) || 'null')
+  return JSON.parse(localStorage.getItem(name) || 'null')
 }
 
 // 生成tag，并添加属性（为hash表）
 function tag(tagName, attr) {
-    var element = document.createElement(tagName)
-    for (var key in attr) {
-        element[key] = attr[key]
-
-    }
-    return element
+  var element = document.createElement(tagName)
+  for (var key in attr) {
+    element[key] = attr[key]
+  }
+  return element
 }
 
 function createSpan(content) {
-    return span = tag('span', { textContent: content, className: 'text' })
+  return (span = tag('span', { textContent: content, className: 'text' }))
 }
 
 function createButton(keyId) {
-    var button = tag('button', { textContent: 'E', id: keyId })
+  var button = tag('button', { textContent: 'E', id: keyId })
 
-    button.onclick = function(onclickEvent) {
-        // 获取用户点击的编辑按钮的id，button是一个容器，页面呈现时循环遍历已经走完，永远保存最后一个key
-        // 因此需要一个按键事件来定位到所需key
-        var buttonKey = onclickEvent.target.id
+  button.onclick = function(onclickEvent) {
+    // 获取用户点击的编辑按钮的id，button是一个容器，页面呈现时循环遍历已经走完，永远保存最后一个key
+    // 因此需要一个按键事件来定位到所需key
+    var buttonKey = onclickEvent.target.id
 
-        // 弹出窗口，获取用户的输入信息
-        var newWebsite = prompt('请输入你想更新的网址：')
+    // 弹出窗口，获取用户的输入信息
+    var newWebsite = prompt('请输入你想更新的网址：')
 
-        // hash变更
-        hash[buttonKey] = newWebsite
+    // hash变更
+    hash[buttonKey] = newWebsite
 
-        // 获取变更后的网址图标
-        var img = onclickEvent.target.previousSibling
-        img.src = 'http://' + hash[keyId] + '/favicon.ico'
-        img.onerror = function(event) {
-            event.target.src = 'https://i.loli.net/2019/03/16/5c8bf7dfcefcc.png'
-            event.target.style = 'opacity:0.2'
-        }
-
-        // 将用户变更的网址，即变更的hash存到浏览器中的localStorage
-        localStorage.setItem('hashUpdater', JSON.stringify(hash))
+    // 获取变更后的网址图标
+    var img = onclickEvent.target.previousSibling
+    img.src = 'http://' + hash[keyId] + '/favicon.ico'
+    img.onerror = function(event) {
+      event.target.src = 'https://i.loli.net/2019/03/16/5c8bf7dfcefcc.png'
+      event.target.style = 'opacity:0.2'
     }
 
-    return button
+    // 将用户变更的网址，即变更的hash存到浏览器中的localStorage
+    localStorage.setItem('hashUpdater', JSON.stringify(hash))
+  }
+
+  return button
 }
 
 function createImg(domain) {
-    var img = tag('img')
-    if (domain) {
-        img.src = 'http://' + domain + '/favicon.ico'
-    } else {
-        img.src = 'https://i.loli.net/2019/03/16/5c8bf7dfcefcc.png'
-        img.style = 'opacity:0.2'
-    }
-    img.onerror = function(event) {
-        event.target.src = 'https://i.loli.net/2019/03/16/5c8bf7dfcefcc.png'
-        event.target.style = 'opacity:0.2'
-    }
-    return img
+  var img = tag('img')
+  if (domain) {
+    img.src = 'http://' + domain + '/favicon.ico'
+  } else {
+    img.src = 'https://i.loli.net/2019/03/16/5c8bf7dfcefcc.png'
+    img.style = 'opacity:0.2'
+  }
+  img.onerror = function(event) {
+    event.target.src = 'https://i.loli.net/2019/03/16/5c8bf7dfcefcc.png'
+    event.target.style = 'opacity:0.2'
+  }
+  return img
 }
 
-
 function init() {
-    var keys = [
-        ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-        ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-        ['z', 'x', 'c', 'v', 'b', 'n', 'm']
-    ]
+  var keys = [
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+  ]
 
-    var hash = {
-        'q': 'mail.qq.com',
-        'w': 'weibo.com',
-        'e': 'ele.me',
-        'r': 'ruanyifeng.com',
-        't': 'tencent.com',
-        'y': 'youtube.com',
-        'u': 'uestc.edu.cn',
-        'i': 'iqiyi.com',
-        'o': 'opera.com',
-        'p': 'picture.baidu.com/',
-        'a': 'amazon.com',
-        's': '',
-        'z': 'zhihu.com',
-        'v': 'v2ex.com',
-        'c': 'cctv.com',
-        'b': 'bilibili.com',
-        'j': 'javascript.ruanyifeng.com',
-        'h': 'huya.com',
-        'd': 'douyu.com',
-        'n': 'nodejs.cn/',
-        'k': null,
-        'm': null,
-        'l': 'liaoxuefeng.com'
-    }
+  var hash = {
+    q: 'mail.qq.com',
+    w: 'weibo.com',
+    e: 'ele.me',
+    r: 'ruanyifeng.com',
+    t: 'tencent.com',
+    y: 'youtube.com',
+    u: 'uestc.edu.cn',
+    i: 'iqiyi.com',
+    o: 'opera.com',
+    p: 'picture.baidu.com/',
+    a: 'amazon.com',
+    s: '',
+    z: 'zhihu.com',
+    v: 'v2ex.com',
+    c: 'cctv.com',
+    b: 'bilibili.com',
+    j: 'javascript.ruanyifeng.com',
+    h: 'huya.com',
+    d: 'douyu.com',
+    n: 'nodejs.cn/',
+    k: null,
+    m: null,
+    l: 'liaoxuefeng.com'
+  }
 
-    // 将localStorage中的hashUpdater覆盖到初始的hash中，以防止刷新浏览器导致变更信息丢失
-    // 添加'null'防止hashUpdater为空，parse失败
-    var hashInLocalStorage = getFromLocalStorage('hashUpdater')
-    if (hashInLocalStorage) {
-        hash = hashInLocalStorage
-    }
+  // 将localStorage中的hashUpdater覆盖到初始的hash中，以防止刷新浏览器导致变更信息丢失
+  // 添加'null'防止hashUpdater为空，parse失败
+  var hashInLocalStorage = getFromLocalStorage('hashUpdater')
+  if (hashInLocalStorage) {
+    hash = hashInLocalStorage
+  }
 
-    return {
-        'keys': keys,
-        'hash': hash
-    }
+  return {
+    keys: keys,
+    hash: hash
+  }
 }
 
 function generateKeyboard(keys, hash) {
-    for (var i = 0; i < keys.length; i++) {
-        var div = tag('div', null)
+  for (var i = 0; i < keys.length; i++) {
+    var div = tag('div', null)
 
-        keyboard.appendChild(div) // keyboard是div的wrapper的id
+    keyboard.appendChild(div) // keyboard是div的wrapper的id
 
-        for (var j = 0; j < keys[i].length; j++) {
+    for (var j = 0; j < keys[i].length; j++) {
+      // 获取按键
+      var k = keys[i][j]
 
-            // 获取按键
-            var k = keys[i][j]
+      // createSpan(k)
 
-            // createSpan(k)
+      var button = createButton(k)
 
-            var button = createButton(k)
+      // 添加按键图标
+      var img = createImg(hash[k])
 
-            // 添加按键图标
-            var img = createImg(hash[k])
+      var kbd = tag('kbd', { textContent: k.toUpperCase(), className: 'key' })
+      // kdb.appendChild(span)
+      kbd.appendChild(img)
+      kbd.appendChild(button)
 
-            var kbd = tag('kbd', { textContent: k.toUpperCase(), className: 'key' })
-                // kdb.appendChild(span)
-            kbd.appendChild(img)
-            kbd.appendChild(button)
-
-            div.appendChild(kbd)
-
-        }
+      div.appendChild(kbd)
     }
-
+  }
 }
 
 function listenToUser(hash) {
-
-    document.onkeypress = function(onkeypressEvent) {
-        var key = onkeypressEvent.key
-        var website = hash[key]
-        window.open('http://' + website, '_blank')
-            // location.href = 'http://' + website
-    }
+  document.onkeypress = function(onkeypressEvent) {
+    var key = onkeypressEvent.key
+    var website = hash[key]
+    window.open('http://' + website, '_blank')
+    // location.href = 'http://' + website
+  }
 }
